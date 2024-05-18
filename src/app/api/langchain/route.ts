62c3760cfa385a2ac871c3a5c20ed20fd783e3a0
Natsumi.H <natsmy.1211@gmail.com>
2024-05-19 05:49:07 +0800

    initial commit langchain api

---
+++ src/app/api/langchain/route.ts
@@ -0,0 +1,71 @@
+import {
+  Message as VercelChatMessage,
+  StreamingTextResponse,
+  createStreamDataTransformer,
+} from "ai";
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { HttpResponseOutputParser } from "langchain/output_parsers";
+
+export const dynamic = "force-dynamic";
+
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+
+// const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
+const TEMPLATE = `
+You are one of the elder in Singapore who is having some issues in life. You are a Retired music teacher, suffers from arthritis, lives alone.
+You are Creative, sensitive, enjoys arts and crafts.
+You feel isolated due to physical pain preventing her from attending social activities.
+Since this is a chance to roleplay my volunteer skills to those elderies, measuring if I can be a good volunteer to them, I will be the volunteer who is trying to help you.
+{chat_history}
+
+player: {input}
+elder:`;
+
+export async function POST(req: Request) {
+  console.log("api");
+
+  try {
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
+
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+
+    const currentMessageContent = messages.at(-1).content;
+
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+
+    const model = new ChatOpenAI({
+      apiKey: process.env.OPENAI_API_KEY!,
+      model: "gpt-3.5-turbo",
+      temperature: 0.8,
+    });
+
+    /**
+     * Chat models stream message chunks rather than bytes, so this
+     * output parser handles serialization and encoding.
+     */
+    const parser = new HttpResponseOutputParser();
+
+    const chain = prompt.pipe(model).pipe(parser);
+
+    // Convert the response into a friendly text-stream
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
+
+    // Respond with the stream
+    return new StreamingTextResponse(
+      stream.pipeThrough(createStreamDataTransformer())
+    );
+  } catch (e: any) {
+    return Response.json({ error: e.message }, { status: e.status ?? 500 });
+  }
+}
